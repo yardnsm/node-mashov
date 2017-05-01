064d7a07b4aefd65138ff2128cd193547287686b
Yarden Sod-Moriah <yardnsm@gmail.com>
2017-05-01 20:29:49 +0300

    Fix login

--- test/client.js
+++ test/client.js
@@ -1,6 +1,7 @@
 import moxios from 'moxios';
 import { endpoints } from '../lib/api-config';
 import Client from '../lib/client';
+import loginFixture from './fixtures/login'
 
 beforeEach(() => {
   moxios.install();
@@ -14,8 +15,8 @@ describe('Client', () => {
 
   describe('login', () => {
 
-    it('should fail if details are wrong', () => {
-      moxios.stubRequest(endpoints.schools, {
+    it('should fail if details are wrong', (done) => {
+      moxios.stubRequest(endpoints.login, {
         status: 401
       });
 
@@ -26,7 +27,24 @@ describe('Client', () => {
         year: 2017
       });
 
-      return expect(client.login()).rejects;
+      client.login().catch(done);
+    });
+
+  it('should resolve if details are correct', (done) => {
+      moxios.stubRequest(endpoints.login, {
+        status: 200,
+        response: loginFixture.plain,
+        headers: loginFixture.headers
+      });
+
+      const client = new Client({
+        username: 'correctUsername',
+        password: 'correctPassword',
+        school: {},
+        year: 2017
+      });
+
+      client.login().then(done);
     });
   });
 });
